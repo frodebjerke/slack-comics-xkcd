3bcc245a8d7bef7fcf61a17fd75f15070080ac10
Frode Bjerke <frode.bjerke@gmail.com>
2016-01-20 22:38:21 +0100

    Make this a boilerplate express react app

--- server/views/home.react.js
+++ server/views/home.react.js
@@ -6,12 +6,12 @@ export default class Home extends React.Component {
   render() {
     return <html>
       <head>
-        <title>Frode Bjerke</title>
+        <title>B O I L E R P L A T E</title>
         <meta name="viewport" content="width=device-width" />
         <link href="assets/styles.css" rel="stylesheet" />
       </head>
       <body>
-        <section id="frodebjerke" />
+        <section id="frontend" />
         <script src="assets/scripts.js" />
       </body>
     </html>
